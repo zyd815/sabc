9dd5efc4ecebedd1c71e780330d9dc0f5e7caf4e
zyd815 <zyd815@163.com>
2016-08-15 13:18:46 +0800

    Initial commit

---
+++ src/main/java/com/springapp/mvc/connection.java
@@ -0,0 +1,31 @@
+package com.springapp.mvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by ABCDC on 2016/8/15.
+ */
+public class connection {
+    public static void main(String [] args){
+        try{
+            //加载MySql的驱动类
+            Class.forName("com.sybase.jdbc3.jdbc.SybDriver") ;
+        }catch(ClassNotFoundException e){
+            System.out.println("找不到驱动程序类 ，加载驱动失败！");
+            e.printStackTrace() ;
+        }
+        String url = "jdbc:sybase:Tds:10.6.1.178:4200/ftzmiss" ;
+        String username = "ftzmiss" ;
+        String password = "kfrbftzmiss" ;
+        try{
+            Connection con =
+                    DriverManager.getConnection(url, username, password) ;
+        }catch(SQLException se){
+            System.out.println("数据库连接失败！");
+            se.printStackTrace() ;
+        }
+    }
+
+}
